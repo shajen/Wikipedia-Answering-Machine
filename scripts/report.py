0cf8b212f715f649b9e6b4661d21b7de22cba1d2
shajen <shajen@shajen.pl>
2019-07-03 18:29:44 +0200

    Added report script.

---
+++ scripts/report.py
@@ -0,0 +1,43 @@
+from data.models import *
+
+import argparse
+import logging
+import os
+import shlex
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+import tools.logger
+import tools.report_manager
+
+def run(*args):
+    try:
+        args = shlex.split(args[0])
+    except IndexError:
+        args = []
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--top_answers", help="count selected top answers", type=str, default='1,10,100', metavar="n1,n2,n3...")
+    parser.add_argument("-mp", "--method_patterns", help="print only method with patterns", type=str, default='', metavar="pattern")
+    parser.add_argument("-qap", "--question_answer_position", help="print questions start from answer position", type=int, default=100, metavar='position')
+    parser.add_argument("-qab", "--question_better", help="print questions answers from better to worst way", action='store_true')
+    parser.add_argument("-qc", "--question_count", help="print n questions", type=int, default=3, metavar='n')
+    parser.add_argument("-snf", "--show_not_found", help="show not found answers", action='store_true')
+    parser.add_argument("-nc", "--no_color", help="switch off color", action='store_true')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    args = parser.parse_args(args)
+
+    tools.logger.configLogger(args.verbose)
+
+    tops = [int(x) for x in args.top_answers.split(',')]
+    qam = tools.report_manager.ReportManager(args.no_color)
+    data = {
+        'tops' : tops,
+        'methodPatterns' : args.method_patterns,
+        'questionAnswerPosition' : args.question_answer_position,
+        'questionBetter' : args.question_better,
+        'questionCount' : args.question_count,
+        'showNotFound' : args.show_not_found,
+        }
+    qam.process(data)
---
+++ scripts/tools/report_manager.py
@@ -0,0 +1,96 @@
+from data.models import *
+
+import logging
+import sys
+from collections import defaultdict
+from termcolor import colored, cprint
+from functools import reduce
+
+class ReportManager():
+    def __init__ (self, no_color):
+        self.no_color = no_color
+
+    def methodColor(self, name):
+        if self.no_color:
+            return name
+        return colored(name, 'yellow', attrs={"bold"})
+
+    def splitColor(self, splitString):
+        if self.no_color:
+            return splitString
+        return colored(splitString, 'red', attrs={"bold"})
+
+    def process(self, args):
+        logging.info("process with top answers (%s)" % ', '.join(str(x) for x in args['tops']))
+        self.printErrorRate(args)
+        self.printQuestions(args)
+
+    def calculateMethodsQuestionsPositions(self, args):
+        methods_questions_positions = defaultdict(lambda: defaultdict(set))
+        for solution in Solution.objects.all():
+            position = solution.position
+            method_id = solution.method_id
+            question_id = solution.answer.question.id
+            if not args['showNotFound'] and position == 10**9:
+                continue
+            methods_questions_positions[method_id][question_id].add(position)
+        return methods_questions_positions
+
+    def printErrorRate(self, args):
+        methods_questions_positions = self.calculateMethodsQuestionsPositions(args)
+        LEN = 60
+        sys.stdout.write(' ' * LEN + '     #')
+        for t in args['tops']:
+            sys.stdout.write('  %6d' % t)
+        sys.stdout.write('\n')
+        for method in Method.objects.filter(name__contains=args['methodPatterns']).order_by('name').all():
+            answersCount = method.solution_set.count()
+            if answersCount == 0:
+                continue
+            if len(method.name) <= LEN:
+                sys.stdout.write("%s %5d" % (self.methodColor(method.name.ljust(LEN)), answersCount))
+            else:
+                sys.stdout.write("%s\n" % self.methodColor(method.name))
+                sys.stdout.write('%s %5d' % (' ' * LEN, answersCount))
+            positions = methods_questions_positions[method.id].values()
+            positions = [list(x) for x in positions]
+            positions = reduce(lambda x, y: x + y, positions)
+            for t in args['tops']:
+                corrected = len(list(filter(lambda x: x <= t, positions)))
+                sys.stdout.write("  %.4f" % (0 if answersCount == 0 else float(corrected)/answersCount))
+            sys.stdout.write('\n')
+
+    def printQuestions(self, args):
+        if args['questionCount'] <= 0:
+            return
+        print('')
+        articles = defaultdict(set)
+        for method in Method.objects.order_by('name').all():
+            print("method %s" % self.methodColor(method.name))
+            data = defaultdict(list)
+            questions_positions = defaultdict(set)
+            for solution in method.solution_set.all():
+                questions_positions[solution.answer.question.id].add(solution.position)
+            for question_id in questions_positions:
+                positions = list(questions_positions[question_id])
+                data[max(positions)].append((question_id, positions))
+            printedCount = 0
+            for pos, value in sorted(data.items(), reverse=not args['questionBetter']):
+                if (args['questionBetter'] and pos < args['questionAnswerPosition']) or (not args['questionBetter'] and pos > args['questionAnswerPosition']):
+                    continue
+                for (question_id, positions) in value:
+                    if printedCount >= args['questionCount']:
+                        break
+                    question = Question.objects.get(id=question_id)
+                    print("#QT %s" % question.name)
+                    for answer in question.answer_set.all():
+                        print("#AT %s" % answer.article.title)
+                    print("#QI %s" % question_id)
+                    for answer in question.answer_set.all():
+                        print("#AI %s" % answer.article.id)
+                    print("#AP %s" % ', '.join(str(p) for p in positions))
+                    print(self.splitColor("-" * 80))
+                    printedCount += 1
+                if printedCount >= args['questionCount']:
+                    break
+            print("")
