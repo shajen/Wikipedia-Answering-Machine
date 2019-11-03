dab24a262b93c3257793f6491d4d7e4f703372dd
shajen <shajen@shajen.pl>
2019-11-03 19:01:46 +0100

    Added model enabling paremeters.

--- 06-resolve.sh
+++ 06-resolve.sh
@@ -5,12 +5,14 @@ THREADS=$(grep -c ^processor /proc/cpuinfo)
 
 pushd $ROOT_PATH
 METHOD="test"
-NEIGHBORS="0,10,20"
+NEIGHBORS="0"
 MINIMAL_IDF="0,1.6094379124341003,3.2188758248682006,4.8283137373023015,6.437751649736401"
 PF="1,2,3,4"
+QUESTIONS="100000"
+MODELS="-tm -vm"
 
-./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vv -n 1"
-./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vv -n 2"
-./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vv -n 1 -T"
-./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vv -n 2 -T"
+./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vvv -q $QUESTIONS $MODELS -n 1"
+./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vvv -q $QUESTIONS $MODELS -n 2"
+./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vvv -q $QUESTIONS $MODELS -n 1 -T"
+./manage.py runscript resolve --script-args="-t $THREADS -m $METHOD -N $NEIGHBORS -mwiw $MINIMAL_IDF -pf $PF -dti 3 -vvv -q $QUESTIONS $MODELS -n 2 -T"
 popd
--- scripts/resolve.py
+++ scripts/resolve.py
@@ -18,7 +18,7 @@ import calculators.tf_idf_weight_calculator
 import calculators.weight_comparator
 import tools.logger
 
-def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items, neighbors, minimal_word_idf_weights, power_factors):
+def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors):
     tf_idf_calculator = calculators.tf_idf_weight_calculator.TfIdfWeightCalculator(debug_top_items, ngram)
     # links_wc = calculators.links_weight_calculator.LinksWeightCalculator(debug_top_items)
     # categories_wc = calculators.categories_weight_calculator.CategoriesWeightCalculator(debug_top_items)
@@ -36,11 +36,13 @@ def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items,
                 for minimal_word_idf_weight in minimal_word_idf_weights:
                     comparators = []
                     method_name = '%s, mwiw: %.2f' % (method, minimal_word_idf_weight)
-                    for power_factor in power_factors:
-                        comparators.append(calculators.weight_comparator.TfIdfWeightComparator(method_name, neighbor, power_factor))
-                    comparators.append(calculators.weight_comparator.CosineWeightComparator(method_name, neighbor))
-                    comparators.append(calculators.weight_comparator.EuclideanWeightComparator(method_name, neighbor))
-                    comparators.append(calculators.weight_comparator.CityblockWeightComparator(method_name, neighbor))
+                    if tfidf_models:
+                        for power_factor in power_factors:
+                            comparators.append(calculators.weight_comparator.TfIdfWeightComparator(method_name, neighbor, power_factor))
+                    if vector_models:
+                        comparators.append(calculators.weight_comparator.CosineWeightComparator(method_name, neighbor))
+                        comparators.append(calculators.weight_comparator.EuclideanWeightComparator(method_name, neighbor))
+                        comparators.append(calculators.weight_comparator.CityblockWeightComparator(method_name, neighbor))
 
                     comparators = list(filter(lambda comparator: not comparator.has_already_solutions(question), comparators))
                     if comparators:
@@ -53,7 +55,7 @@ def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items,
         except queue.Empty:
             break
 
-def start(questions, num_threads, method_name, is_title, ngram, debug_top_items, neighbors, minimal_word_idf_weights, power_factors):
+def start(questions, num_threads, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors):
     logging.info('start')
     logging.info('threads: %d' % num_threads)
     logging.info('debug_top_items: %d' % debug_top_items)
@@ -69,7 +71,7 @@ def start(questions, num_threads, method_name, is_title, ngram, debug_top_items,
 
     threads = []
     for i in range(num_threads):
-        thread = multiprocessing.Process(target=resolve_questions, args=(questions_queue, method_name, is_title, ngram, debug_top_items, neighbors, minimal_word_idf_weights, power_factors))
+        thread = multiprocessing.Process(target=resolve_questions, args=(questions_queue, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors))
         thread.start()
         threads.append(thread)
 
@@ -87,6 +89,8 @@ def run(*args):
     parser.add_argument("-q", "--questions", help="solve first n questions", type=int, default=10**9, metavar="n")
     parser.add_argument("-T", "--title", help="calculate based on articles title not content", action='store_true')
     parser.add_argument('-n', '--ngram', help="use ngram mode", type=int, default=1, metavar="ngram")
+    parser.add_argument("-tm", "--tfidf_models", help="use td-idf models", action='store_true')
+    parser.add_argument("-vm", "--vector_models", help="use vector models", action='store_true')
     parser.add_argument('-m', '--method', help="method name to make unique in database", type=str, default='', metavar="method")
     parser.add_argument("-dti", "--debug_top_items", help="print top n items in debug", type=int, default=3, metavar="int")
     parser.add_argument("-N", "--neighbors", help="count tf-idf in every n neighboring words tuple in articles", type=str, default='0', metavar="0,10,20")
@@ -107,4 +111,4 @@ def run(*args):
     neighbors = list(map(lambda x: int(x), args.neighbors.split(',')))
     minimal_word_idf_weights = list(map(lambda x: float(x), args.minimal_word_idf_weights.split(',')))
     power_factors = list(map(lambda x: int(x), args.power_factors.split(',')))
-    start(questions, args.threads, method_name, args.title, args.ngram, args.debug_top_items, neighbors, minimal_word_idf_weights, power_factors)
+    start(questions, args.threads, method_name, args.title, args.ngram, args.debug_top_items, args.tfidf_models, args.vector_models, neighbors, minimal_word_idf_weights, power_factors)
