f9d5a579ac7714e9cc3459a1d3b751967cdeaaa5
shajen <shajen@shajen.pl>
2019-12-07 18:50:46 +0100

    Added multithread support in word2vec model. Reorganise some debugs.

--- scripts/calculators/word2vec_weight_calculator.py
+++ scripts/calculators/word2vec_weight_calculator.py
@@ -2,47 +2,39 @@ from collections import defaultdict
 from data.models import *
 from functools import reduce
 from termcolor import colored
-import calculators.weight_calculator
 import gensim.models
 import logging
-import math
 import numpy as np
-import re
 import scipy.spatial
 
-class Word2VecWeightCalculator(calculators.weight_calculator.WeightCalculator):
+class Word2VecWeightCalculator():
     def __init__(self, debug_top_items, model_file):
-        super().__init__(debug_top_items)
-
+        self.__debug_top_items = debug_top_items
         logging.info('loading word2vec model')
         self.__word2vec_model = gensim.models.KeyedVectors.load(model_file)
-
         logging.info('start reading stop words')
         self.__stop_words = set(Word.objects.filter(is_stop_word=True).values_list('id', flat=True))
         logging.info('stop words: %d' % len(self.__stop_words))
-
-        logging.info('finish')
 
     def __get_similar_words_id(self, ids, topn):
         similar_words = []
         for word in Word.objects.filter(id__in=ids).values_list('value', flat=True):
             try:
-                # logging.debug(word)
+                logging.debug(word)
                 for (similar_word, distance) in self.__word2vec_model.most_similar(word, topn=topn):
-                    # logging.debug('  %s %.6f' % (similar_word, distance))
+                    logging.debug('  %s %.6f' % (similar_word, distance))
                     similar_words.append(similar_word)
             except KeyError:
                 pass
         return list(Word.objects.filter(value__in=similar_words).values_list('id', flat=True)) + ids
 
     def __get_words_changed_forms_id(self, ids):
-        return list(WordForm.objects.filter(base_word_id__in=ids).values_list('changed_word_id', flat=True))# + ids
+        return list(WordForm.objects.filter(base_word_id__in=ids).values_list('changed_word_id', flat=True)) + ids
 
     def __filter_stop_words_id(self, ids):
         return list(filter(lambda id: id not in self.__stop_words, ids))
 
     def __words_id_to_words_base_forms_id(self, ids):
-        ids = self.__filter_stop_words_id(ids)
         ids = list(filter(lambda id: id in self.__word_id_to_word_base_form_id, ids))
         ids = list(map(lambda id: self.__word_id_to_word_base_form_id[id], ids))
         ids = list(reduce(lambda x, y: x + y, ids, []))
@@ -67,47 +59,42 @@ class Word2VecWeightCalculator(calculators.weight_calculator.WeightCalculator):
         return articles_words_id
 
     def __load_words_id_vectors(self, ids):
-        logging.info('start reading words vectors')
         self.__word_id_to_vector = {}
         for (id, word) in Word.objects.filter(is_stop_word=False).filter(id__in=ids).values_list('id', 'value'):
             try:
                 self.__word_id_to_vector[id] = self.__word2vec_model.get_vector(word)
             except KeyError:
                 pass
-        logging.info('words vectors: %d' % len(self.__word_id_to_vector))
 
     def __load_words_id_base_form_id(self, ids):
-        logging.info('start reading words base forms')
         self.__word_id_to_word_base_form_id = defaultdict(list)
         for (base_word_id, changed_word_id) in WordForm.objects.filter(changed_word_id__in=ids).values_list('base_word_id', 'changed_word_id'):
             self.__word_id_to_word_base_form_id[changed_word_id].append(base_word_id)
         for id in ids:
             self.__word_id_to_word_base_form_id[id].append(id)
-        logging.info('words base forms: %d' % len(self.__word_id_to_word_base_form_id))
 
     def __prepare_data(self, question, is_title, topn):
-        logging.info('preparing question')
-
         question_words_id = self.__get_question_words_id(question)
-        logging.info('words count %d' % len(question_words_id))
+        question_words_id = self.__filter_stop_words_id(question_words_id)
+        logging.info('words count: %d' % len(set(question_words_id)))
 
         self.__load_words_id_base_form_id(question_words_id)
         question_words_id = self.__words_id_to_words_base_forms_id(question_words_id)
-        xxx = question_words_id
-        logging.info('words base forms count %d' % len(question_words_id))
+        logging.info('words base forms count: %d' % len(set(question_words_id)))
 
-        question_words_id = self.__get_similar_words_id(question_words_id, topn)
-        logging.info('similar words count %d' % len(question_words_id))
+        similar_words_id = self.__get_similar_words_id(question_words_id, topn)
+        logging.info('similar words count: %d' % len(set(similar_words_id)))
 
-        question_words_id = self.__get_words_changed_forms_id(question_words_id) + xxx
-        logging.info('changed form similar words count %d' % len(question_words_id))
+        similar_words_id = self.__get_words_changed_forms_id(similar_words_id)
+        logging.info('changed form similar words count: %d' % len(set(similar_words_id)))
 
-        self.__load_words_id_vectors(question_words_id)
-        self.__load_words_id_base_form_id(question_words_id)
-        question_data = self.__words_id_to_data(xxx)
-        question_data = np.array(question_data)
+        self.__load_words_id_vectors(similar_words_id)
+        logging.info('words vectors: %d' % len(set(self.__word_id_to_vector)))
+        self.__load_words_id_base_form_id(similar_words_id)
+        logging.info('words base forms: %d' % len(set(self.__word_id_to_word_base_form_id)))
+
         logging.info('reading articles')
-        articles_words_id = self.__get_articles_words_id(question_words_id, is_title)
+        articles_words_id = self.__get_articles_words_id(similar_words_id, is_title)
         articles_id = []
         articles_data = []
         logging.info('preparing articles data')
@@ -117,6 +104,7 @@ class Word2VecWeightCalculator(calculators.weight_calculator.WeightCalculator):
         articles_data = np.array(articles_data)
         logging.info('articles size: %s' % str(articles_data.shape))
 
+        question_data = np.array(self.__words_id_to_data(question_words_id))
         return (question_data, articles_id, articles_data)
 
     def __calculate_distances(self, question_data, articles_data):
@@ -136,24 +124,21 @@ class Word2VecWeightCalculator(calculators.weight_calculator.WeightCalculator):
     def __upload_positions(self, question, articles_id, distances, method):
         scores = np.argsort(np.argsort(distances))
         corrected_articles_id = list(question.answer_set.all().values_list('article_id', flat=True))
-        for position in range(0, self.debug_top_items):
+        for position in range(0, self.__debug_top_items):
             self.__print(corrected_articles_id, position, articles_id, scores, distances)
 
         for answer in question.answer_set.all():
             i = articles_id.index(answer.article_id)
             position = scores[i]
-            if (position >= self.debug_top_items):
+            if (position >= self.__debug_top_items):
                 self.__print(corrected_articles_id, position, articles_id, scores, distances)
             Solution.objects.create(position=position+1, answer=answer, method=method)
         logging.info('')
 
-    def calculate(self, questions, method_name, is_title):
-        Method.objects.filter(name__contains='type: xxx').delete()
-        method, created = Method.objects.get_or_create(name=('%s, type: xxx' % method_name))
-
-        for question in [questions[2]]:
-            logging.info('calculating')
-            logging.warning(self.__colored('question: %s' % (question), 'yellow'))
-            (question_data, articles_id, articles_data) = self.__prepare_data(question, is_title, 10)
-            distances = self.__calculate_distances(question_data, articles_data)
-            self.__upload_positions(question, articles_id, distances, method)
+    def calculate(self, question, method_name, is_title, topn):
+        logging.info('calculating')
+        method, created = Method.objects.get_or_create(name=method_name)
+        logging.warning(self.__colored('question: %s' % (question), 'yellow'))
+        (question_data, articles_id, articles_data) = self.__prepare_data(question, is_title, topn)
+        distances = self.__calculate_distances(question_data, articles_data)
+        self.__upload_positions(question, articles_id, distances, method)
--- scripts/resolve.py
+++ scripts/resolve.py
@@ -19,7 +19,7 @@ import calculators.weight_comparator
 import calculators.word2vec_weight_calculator
 import tools.logger
 
-def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors):
+def resolve_questions_tf_idf(questions_queue, method, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors):
     tf_idf_calculator = calculators.tf_idf_weight_calculator.TfIdfWeightCalculator(debug_top_items, ngram)
     # links_wc = calculators.links_weight_calculator.LinksWeightCalculator(debug_top_items)
     # categories_wc = calculators.categories_weight_calculator.CategoriesWeightCalculator(debug_top_items)
@@ -58,37 +58,61 @@ def resolve_questions(questions_queue, method, is_title, ngram, debug_top_items,
 
 def start_tfidf(questions, num_threads, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors):
     db.connections.close_all()
+    logging.info('neighbors: %s' % neighbors)
+    logging.info('minimal_word_idf_weights: %s' % minimal_word_idf_weights)
+    logging.info('power_factors: %s' % power_factors)
+    method_name = '%s, ngram: %d' % (method_name, ngram)
+    logging.info('method_name: %s' % method_name)
     questions_queue = multiprocessing.Queue()
     for question in questions:
         questions_queue.put(question)
 
     threads = []
     for i in range(num_threads):
-        thread = multiprocessing.Process(target=resolve_questions, args=(questions_queue, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors))
+        thread = multiprocessing.Process(target=resolve_questions_tf_idf, args=(questions_queue, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors))
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
 
-def start_word2vec(questions, num_threads, method_name, is_title, debug_top_items, word2vec_model):
+def resolve_questions_word2vec(questions_queue, method_name, is_title, debug_top_items, topn, word2vec_model):
+    word2vec_calculator = calculators.word2vec_weight_calculator.Word2VecWeightCalculator(debug_top_items, word2vec_model)
+    while True:
+        try:
+            question = questions_queue.get(timeout=1)
+            word2vec_calculator.calculate(question, method_name, is_title, topn)
+        except queue.Empty:
+            break
+
+def start_word2vec(questions, num_threads, method_name, is_title, debug_top_items, topn, word2vec_model):
+    db.connections.close_all()
     logging.getLogger("gensim").setLevel(logging.WARNING)
     logging.getLogger("smart_open.smart_open_lib").setLevel(logging.WARNING)
-    word2vec_calculator = calculators.word2vec_weight_calculator.Word2VecWeightCalculator(debug_top_items, word2vec_model)
-    word2vec_calculator.calculate(questions, method_name, is_title)
+    logging.info('topn: %s' % topn)
+    method_name = '%s, topn: %d, type: word2vec' % (method_name, topn)
+    logging.info('method_name: %s' % method_name)
+    questions_queue = multiprocessing.Queue()
+    for question in questions:
+        questions_queue.put(question)
 
-def start(questions, num_threads, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, word2vec_model, neighbors, minimal_word_idf_weights, power_factors):
+    threads = []
+    for i in range(num_threads):
+        thread = multiprocessing.Process(target=resolve_questions_word2vec, args=(questions_queue, method_name, is_title, debug_top_items, topn, word2vec_model))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+def start(questions, num_threads, method_name, is_title, ngram, debug_top_items, topn, tfidf_models, vector_models, word2vec_model, neighbors, minimal_word_idf_weights, power_factors):
     logging.info('start')
     logging.info('threads: %d' % num_threads)
     logging.info('debug_top_items: %d' % debug_top_items)
-    logging.info('method_name: %s' % method_name)
-    logging.info('neighbors: %s' % neighbors)
-    logging.info('minimal_word_idf_weights: %s' % minimal_word_idf_weights)
-    logging.info('power_factors: %s' % power_factors)
     if tfidf_models or vector_models:
         start_tfidf(questions, num_threads, method_name, is_title, ngram, debug_top_items, tfidf_models, vector_models, neighbors, minimal_word_idf_weights, power_factors)
     if word2vec_model:
-        start_word2vec(questions, num_threads, method_name, is_title, debug_top_items, word2vec_model)
+        start_word2vec(questions, num_threads, method_name, is_title, debug_top_items, topn, word2vec_model)
     logging.info('finish')
 
 def run(*args):
@@ -106,6 +130,7 @@ def run(*args):
     parser.add_argument("-w2vm", "--word2vec_model", help="use word2vec model", type=str, default='', metavar="file")
     parser.add_argument('-m', '--method', help="method name to make unique in database", type=str, default='', metavar="method")
     parser.add_argument("-dti", "--debug_top_items", help="print top n items in debug", type=int, default=3, metavar="int")
+    parser.add_argument("-tn", "--topn", help="use n nearest words in word2vec model", type=int, default=10, metavar="n")
     parser.add_argument("-N", "--neighbors", help="count tf-idf in every n neighboring words tuple in articles", type=str, default='0', metavar="0,10,20")
     parser.add_argument("-mwiw", "--minimal_word_idf_weights", help="use only words with idf weight above", type=str, default='0.0', metavar="0.0,1.6,3.2")
     parser.add_argument("-pf", "--power_factors", help="use to sum words weight in count article weight", type=str, default='4', metavar="1,2,3,4")
@@ -118,10 +143,10 @@ def run(*args):
     dirPath = os.path.dirname(os.path.realpath(__file__))
     commit_hash = subprocess.check_output(['git', '-C', dirPath, 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
     commit_datetime = subprocess.check_output(['git', '-C', dirPath, 'log', '-1', '--format=%at']).decode('ascii').strip()
-    method_name = 'git: %s_%s, title: %d, ngram: %d' % (commit_datetime, commit_hash, args.title, args.ngram)
+    method_name = 'git: %s_%s, title: %d' % (commit_datetime, commit_hash, args.title)
     if args.method:
         method_name = 'name: %s, %s' % (args.method, method_name)
     neighbors = list(map(lambda x: int(x), args.neighbors.split(',')))
     minimal_word_idf_weights = list(map(lambda x: float(x), args.minimal_word_idf_weights.split(',')))
     power_factors = list(map(lambda x: int(x), args.power_factors.split(',')))
-    start(questions, args.threads, method_name, args.title, args.ngram, args.debug_top_items, args.tfidf_models, args.vector_models, args.word2vec_model, neighbors, minimal_word_idf_weights, power_factors)
+    start(questions, args.threads, method_name, args.title, args.ngram, args.debug_top_items, args.topn, args.tfidf_models, args.vector_models, args.word2vec_model, neighbors, minimal_word_idf_weights, power_factors)
