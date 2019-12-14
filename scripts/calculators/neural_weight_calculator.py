72df64ee9c117f4a651af0e857ed29e27a264b25
shajen <shajen@shajen.pl>
2019-12-14 01:45:05 +0100

    Added some debug, optimized preparing data.

--- scripts/calculators/neural_weight_calculator.py
+++ scripts/calculators/neural_weight_calculator.py
@@ -44,38 +44,43 @@ class NeuralWeightCalculator():
     def __words2vec(self, words):
         return list(map(lambda word: self.__word2vec(word), words))
 
-    def __prepareArticle(self, articles_id, words, is_title):
+    def __prepareArticle(self, articles_id, words, name, is_title):
         logging.debug('preparing %d articles, top words: %d, title: %d' %(len(articles_id), words, is_title))
-        articles_words = defaultdict(lambda : [None] * words)
-        total = ArticleOccurrence.objects.filter(article_id__in=articles_id, is_title=is_title).count()
-        logging.debug("total records: %d" % total)
-        for (article_id, word_id, positions) in ArticleOccurrence.objects.filter(article_id__in=articles_id, is_title=is_title).values_list('article_id', 'word_id', 'positions'):
-            for position in positions.split(','):
-                position = int(position)
-                if (position <= words):
-                    articles_words[article_id][position - 1] = word_id
         data = []
-        logging.debug("words2vec")
+        total = len(articles_id)
+        current = 0
+        step = round(total / (10 if is_title else 100))
         for article_id in articles_id:
-            data.append(self.__words2vec(articles_words[article_id]))
-        data = np.array(data)
-        logging.debug("%s" % str(data.shape))
-        return data
+            current += 1
+            if current % step == 0:
+                logging.debug("progress: %d/%d (%.2f %%)" % (current, total, current / total * 100))
+            article_words = [None] * words
+            for (word_id, positions) in ArticleOccurrence.objects.filter(article_id=article_id, is_title=is_title).values_list('word_id', 'positions').iterator():
+                for position in positions.split(','):
+                    position = int(position)
+                    if (position <= words):
+                        article_words[position - 1] = word_id
+            data.append(self.__words2vec(article_words))
+        self.__save_file(name, np.array(data))
 
     def __prepareQuestion(self, questions_id, words):
         logging.debug('preparing %d questions, top words: %d' %(len(questions_id), words))
-        questions_words = defaultdict(lambda : [None] * words)
-        total = QuestionOccurrence.objects.filter(question_id__in=questions_id).count()
-        logging.debug("total records: %d" % total)
-        for (question_id, word_id, positions) in QuestionOccurrence.objects.filter(question_id__in=questions_id).values_list('question_id', 'word_id', 'positions'):
-            for position in positions.split(','):
-                position = int(position)
-                if (position <= words):
-                    questions_words[question_id][position - 1] = word_id
         data = []
-        logging.debug("words2vec")
+        total = len(questions_id)
+        current = 0
+        step = round(total / 10)
         for question_id in questions_id:
-            data.append(self.__words2vec(questions_words[question_id]))
+            current += 1
+            if (current % step == 0):
+                logging.debug("progress: %d/%d (%.2f %%)" % (current, total, current / total * 100))
+            question_words = [None] * words
+            for (word_id, positions) in QuestionOccurrence.objects.filter(question_id=question_id).values_list('word_id', 'positions').iterator():
+                for position in positions.split(','):
+                    position = int(position)
+                    if (position <= words):
+                        question_words[position - 1] = word_id
+            data.append(self.__words2vec(question_words))
+
         data = np.array(data)
         logging.debug("data size: %s" % str(data.shape))
         return data
@@ -99,20 +104,16 @@ class NeuralWeightCalculator():
         bad_articles_id = list(Article.objects.exclude(id__in=good_articles_id).order_by('?').values_list('id', flat=True)[:goodBadArticlesRatio * len(good_articles_id)])
         logging.debug('bad articles: %d' % len(bad_articles_id))
 
+        self.__prepareArticle(good_articles_id, articleTitleWords, 'good_articles_title_data', True)
+        self.__prepareArticle(good_articles_id, articleWords, 'good_articles_data', False)
+        self.__prepareArticle(bad_articles_id, articleTitleWords, 'bad_articles_title_data', True)
+        self.__prepareArticle(bad_articles_id, articleWords, 'bad_articles_data', False)
         good_questions_data = self.__prepareQuestion(questions_id, questionWords)
-        good_articles_title_data = self.__prepareArticle(good_articles_id, articleTitleWords, True)
-        good_articles_data = self.__prepareArticle(good_articles_id, articleWords, False)
-        good_target = np.array([1.0] * good_articles_data.shape[0])
+        good_target = np.array([1.0] * good_questions_data.shape[0])
         bad_questions_data = np.random.permutation(np.repeat(good_questions_data, goodBadArticlesRatio, axis=0))
-        bad_articles_title_data = self.__prepareArticle(bad_articles_id, articleTitleWords, True)
-        bad_articles_data = self.__prepareArticle(bad_articles_id, articleWords, False)
-        bad_target = np.array([0.0] * bad_articles_data.shape[0])
+        bad_target = np.array([0.0] * bad_questions_data.shape[0])
 
         self.__save_file('good_questions_data', good_questions_data)
-        self.__save_file('good_articles_title_data', good_articles_title_data)
-        self.__save_file('good_articles_data', good_articles_data)
         self.__save_file('good_target', good_target)
         self.__save_file('bad_questions_data', bad_questions_data)
-        self.__save_file('bad_articles_title_data', bad_articles_title_data)
-        self.__save_file('bad_articles_data', bad_articles_data)
         self.__save_file('bad_target', bad_target)
