e8ab72c7836ef7e63df7c3a85e9d4d65fbf6d8eb
shajen <shajen@shajen.pl>
2019-12-29 01:56:32 +0100

    Added ResultPresenter.

--- scripts/calculators/word2vec_weight_calculator.py
+++ scripts/calculators/word2vec_weight_calculator.py
@@ -61,6 +61,55 @@ class Word2VecModel():
     def get_vector(self, word):
         return self.__word2vec_model.get_vector(word)
 
+class ResultsPresenter():
+    def __colored(text, colour):
+        return colored(text, colour, attrs={'bold'})
+
+    def __get_weight(weight, is_smaller_first):
+        if np.isnan(weight):
+            if is_smaller_first:
+                return float(10**5)
+            else:
+                return 0.0
+        else:
+            return weight
+
+    def __print(corrected_articles_id, position, articles_id, scores, distances, question, method, is_smaller_first):
+        i = np.where(scores == position)[0][0]
+        distance = distances[i]
+        article = Article.objects.get(id=articles_id[i])
+        colour = 'green' if articles_id[i] in corrected_articles_id else 'red'
+        sign = '*' if articles_id[i] in corrected_articles_id else ' '
+        logging.warning(ResultsPresenter.__colored(' %sposition: %6d, distance: %5.4f, article (%7d): %s' % (sign, position+1, distance, article.id, article), colour))
+        return Rate(weight=ResultsPresenter.__get_weight(distance, is_smaller_first), article=article, question=question, method=method)
+
+    def upload_positions(question, articles_id, distances, method, debug_top_items, is_smaller_first):
+        rates = []
+        if is_smaller_first:
+            scores = np.argsort(np.argsort(distances))
+        else:
+            scores = np.argsort(np.argsort(distances)[::-1])
+        corrected_articles_id = list(question.answer_set.all().values_list('article_id', flat=True))
+        logging.warning(ResultsPresenter.__colored('question (%5d): %s' % (question.id, question), 'yellow'))
+        for position in range(0, min(len(distances), debug_top_items)):
+            rates.append(ResultsPresenter.__print(corrected_articles_id, position, articles_id, scores, distances, question, method, is_smaller_first))
+
+        for answer in question.answer_set.all():
+            try:
+                i = articles_id.index(answer.article_id)
+                position = scores[i]
+                if (position >= debug_top_items):
+                    rates.append(ResultsPresenter.__print(corrected_articles_id, position, articles_id, scores, distances, question, method, is_smaller_first))
+                Solution.objects.create(position=position+1, answer=answer, method=method)
+            except:
+                position = 10**5
+                distance = np.nan
+                logging.warning(ResultsPresenter.__colored(' %sposition: %6d, distance: %5.4f, article (%7d): %s' % ('*', position, distance, answer.article.id, answer.article), 'green'))
+                Solution.objects.create(position=position, answer=answer, method=method)
+                rates.append(Rate(weight=ResultsPresenter.__get_weight(distance, is_smaller_first), question=question, article=answer.article, method=method))
+        Rate.objects.bulk_create(rates, ignore_conflicts=True)
+        logging.info('')
+
 class Word2VecWeightCalculator():
     def __init__(self, debug_top_items, word2vec_model, articles_data):
         self.__debug_top_items = debug_top_items
@@ -174,37 +223,6 @@ class Word2VecWeightCalculator():
     def __calculate_distances(self, question_data, articles_data):
         return scipy.spatial.distance.cdist(np.array([question_data]), articles_data, 'cosine')[0]
 
-    def __colored(self, text, colour):
-        return colored(text, colour, attrs={'bold'})
-
-    def __print(self, corrected_articles_id, position, articles_id, scores, distances):
-        i = np.where(scores == position)[0][0]
-        distance = distances[i]
-        article = Article.objects.get(id=articles_id[i])
-        colour = 'green' if articles_id[i] in corrected_articles_id else 'red'
-        sign = '*' if articles_id[i] in corrected_articles_id else ' '
-        logging.warning(self.__colored(' %sposition: %6d, distance: %5.4f, article (%7d): %s' % (sign, position+1, distance, article.id, article), colour))
-
-    def __upload_positions(self, question, articles_id, distances, method):
-        scores = np.argsort(np.argsort(distances))
-        corrected_articles_id = list(question.answer_set.all().values_list('article_id', flat=True))
-        logging.warning(self.__colored('question (%5d): %s' % (question.id, question), 'yellow'))
-        for position in range(0, min(len(distances), self.__debug_top_items)):
-            self.__print(corrected_articles_id, position, articles_id, scores, distances)
-
-        for answer in question.answer_set.all():
-            try:
-                i = articles_id.index(answer.article_id)
-                position = scores[i]
-                if (position >= self.__debug_top_items):
-                    self.__print(corrected_articles_id, position, articles_id, scores, distances)
-                Solution.objects.create(position=position+1, answer=answer, method=method)
-            except:
-                position = 10**9
-                logging.warning(self.__colored(' %sposition: %6d, distance: %5.4f, article (%7d): %s' % ('*', position, 99.99, answer.article.id, answer.article), 'green'))
-                Solution.objects.create(position=position, answer=answer, method=method)
-        logging.info('')
-
     def has_already_solutions(self, question, method_name):
         try:
             answers = Answer.objects.filter(question=question).values_list('id', flat=True)
@@ -219,4 +237,4 @@ class Word2VecWeightCalculator():
         (question_data, articles_id, articles_data) = self.__prepare_data(question, is_title, topn)
         if articles_data.size:
             distances = self.__calculate_distances(question_data, articles_data)
-            self.__upload_positions(question, articles_id, distances, method)
+            ResultsPresenter.upload_positions(question, articles_id, distances, method, self.__debug_top_items, True)
