e24c0073531541fdca1b69c15b2ed73ddd7ac0aa
shajen <shajen@shajen.pl>
2020-01-05 15:17:44 +0100

    Added some list views.

--- data/views.py
+++ data/views.py
@@ -1,3 +1,54 @@
+from data.models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+__OBJECTS_PER_PAGE = 10000
+
+def index(request):
+    return render(request, 'index.html')
+
+def article(request, id):
+    try:
+        article = Article.objects.get(id=id)
+        answers = article.answer_set.select_related('question')
+        return render(request, 'data/article.html', {'article' :  article, 'answers' : answers})
+    except ObjectDoesNotExist:
+        raise Http404('Article %d not found' % id)
+
+def articles(request):
+    order_by = request.GET.get('order_by', 'title')
+    page = request.GET.get('page')
+    objects_per_page = request.GET.get('objects_per_page', __OBJECTS_PER_PAGE)
+    paginator = Paginator(Article.objects.filter(redirected_to=None, content_words_count__gte=1, title_words_count__gte=1).order_by(order_by).all(), objects_per_page)
+    return render(request, 'data/articles.html', {'objects': paginator.get_page(page), 'total_count': paginator.count, 'order_by' : order_by })
+
+def question(request, id):
+    try:
+        question = Question.objects.get(id=id)
+        answers = question.answer_set.select_related('article')
+        return render(request, 'data/question.html', {'question' :  question, 'answers' : answers})
+    except ObjectDoesNotExist:
+       raise Http404('Question %d not found' % id)
+
+def questions(request):
+    order_by = request.GET.get('order_by', 'name')
+    page = request.GET.get('page')
+    objects_per_page = request.GET.get('objects_per_page', __OBJECTS_PER_PAGE)
+    paginator = Paginator(Question.objects.order_by(order_by).all(), objects_per_page)
+    return render(request, 'data/questions.html', {'objects': paginator.get_page(page), 'total_count': paginator.count, 'order_by' : order_by })
+
+def method(request, id):
+    try:
+        return render(request, 'data/method.html', {'method' : Method.objects.get(id=id) })
+    except ObjectDoesNotExist:
+        raise Http404('Method %d not found' % id)
+
+def methods(request):
+    order_by = request.GET.get('order_by', 'name')
+    page = request.GET.get('page')
+    objects_per_page = request.GET.get('objects_per_page', __OBJECTS_PER_PAGE)
+    paginator = Paginator(Method.objects.order_by(order_by).all(), objects_per_page)
+    return render(request, 'data/methods.html', {'objects': paginator.get_page(page), 'total_count': paginator.count, 'order_by' : order_by })
--- mgr/urls.py
+++ mgr/urls.py
@@ -1,21 +1,15 @@
-"""mgr URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+import data.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', data.views.index, name='index'),
+    path('articles/', data.views.articles, name='articles'),
+    path('articles/<int:id>/', data.views.article, name='article'),
+    path('questions/', data.views.questions, name='questions'),
+    path('questions/<int:id>/', data.views.question, name='question'),
+    path('methods/', data.views.methods, name='methods'),
+    path('methods/<int:id>/', data.views.method, name='method'),
 ]
