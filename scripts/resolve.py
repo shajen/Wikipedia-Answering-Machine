89b28942726f667e11967f41e50888a4912651f7
shajen <shajen@shajen.pl>
2020-01-26 19:47:14 +0100

    Ignored ean models in ean model

--- scripts/resolve.py
+++ scripts/resolve.py
@@ -140,8 +140,9 @@ def start(args, questions, method_name):
         model = calculators.deep_averaging_neural_weight_calculator.DeepAveragingNeuralWeightCalculator(data_loader, args.debug_top_items, get_workdir(args.cache_directory, 'nn'), args.neural_model_good_bad_ratio, args.neural_model_method)
         start_learning_model(args, questions, model, '%s, type: dan, %s' % (method_name, learning_count))
     if args.evolutionary_algorithm:
-        model = calculators.evolutionary_algorithm.EvolutionaryAlgorithm(args.debug_top_items, get_workdir(args.cache_directory, 'ea'), args.evolutionary_algorithm_methods_patterns, args.evolutionary_algorithm_exclude_methods_patterns, args.evolutionary_algorithm_population)
-        start_learning_model(args, questions, model, '%s, type: ean, p: %04d, mp: %s, emp: %s' % (method_name, args.evolutionary_algorithm_population, args.evolutionary_algorithm_methods_patterns, args.evolutionary_algorithm_exclude_methods_patterns))
+        exclude_methods = 'ean;' + args.evolutionary_algorithm_exclude_methods_patterns
+        model = calculators.evolutionary_algorithm.EvolutionaryAlgorithm(args.debug_top_items, get_workdir(args.cache_directory, 'ea'), args.evolutionary_algorithm_methods_patterns, exclude_methods, args.evolutionary_algorithm_population)
+        start_learning_model(args, questions, model, '%s, type: ean, p: %04d, mp: %s, emp: %s' % (method_name, args.evolutionary_algorithm_population, args.evolutionary_algorithm_methods_patterns, exclude_methods))
     logging.info('finish')
 
 def get_method_name(args):
